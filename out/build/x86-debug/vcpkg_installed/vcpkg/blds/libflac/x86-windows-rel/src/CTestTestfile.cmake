5ec02fc5f9afcdd29c9b81b7f35fc81d78a24bc9
Kevin38300 <92035284+Kevin38300@users.noreply.github.com>
2025-01-13 11:21:57 +0100

    Initial commit

---
+++ out/build/x86-debug/vcpkg_installed/vcpkg/blds/libflac/x86-windows-rel/src/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/K.Lourenco/source/repos/CMake_Ninja/out/build/x86-debug/vcpkg_installed/vcpkg/blds/libflac/src/1.4.3-00043104ae.clean/src
+# Build directory: C:/Users/K.Lourenco/source/repos/CMake_Ninja/out/build/x86-debug/vcpkg_installed/vcpkg/blds/libflac/x86-windows-rel/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("libFLAC")
+subdirs("libFLAC++")
+subdirs("share/replaygain_analysis")
+subdirs("share/replaygain_synthesis")
+subdirs("share/getopt")
+subdirs("share/utf8")
+subdirs("share/grabbag")
