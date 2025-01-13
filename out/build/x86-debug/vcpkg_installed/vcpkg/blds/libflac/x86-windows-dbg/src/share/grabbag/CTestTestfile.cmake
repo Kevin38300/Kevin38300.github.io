c913b567e947878e49676fa1e8a2744c8ea2bb3c
Kevin38300 <92035284+Kevin38300@users.noreply.github.com>
2025-01-13 12:58:08 +0100

    Delete out/build directory

--- out/build/x86-debug/vcpkg_installed/vcpkg/blds/libflac/x86-windows-dbg/src/share/grabbag/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/K.Lourenco/source/repos/CMake_Ninja/out/build/x86-debug/vcpkg_installed/vcpkg/blds/libflac/src/1.4.3-00043104ae.clean/src/share/grabbag
-# Build directory: C:/Users/K.Lourenco/source/repos/CMake_Ninja/out/build/x86-debug/vcpkg_installed/vcpkg/blds/libflac/x86-windows-dbg/src/share/grabbag
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- out/build/x86-debug/vcpkg_installed/vcpkg/blds/libflac/x86-windows-rel/src/share/getopt/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/K.Lourenco/source/repos/CMake_Ninja/out/build/x86-debug/vcpkg_installed/vcpkg/blds/libflac/src/1.4.3-00043104ae.clean/src/share/getopt
-# Build directory: C:/Users/K.Lourenco/source/repos/CMake_Ninja/out/build/x86-debug/vcpkg_installed/vcpkg/blds/libflac/x86-windows-rel/src/share/getopt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
